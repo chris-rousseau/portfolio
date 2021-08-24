640fa9c21ac89ac32676afadc816f9742d77ea42
chris-rousseau <c.rousseau36@gmail.com>
2021-08-24 10:16:32 +0200

    Contact form

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ContactController extends AbstractController
+{
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function sendEmail(MailerInterface $mailer, Request $request): Response
+    {
+        $form = $this->createFormBuilder()
+            ->add('email', EmailType::class, [
+                'label' => 'Votre adresse mail :'
+            ])
+            ->add('message', TextareaType::class, [
+                'label' => 'Votre message :'
+            ])
+            ->add('send', SubmitType::class, [
+                'label' => 'Envoyer l\'email !'
+            ])
+            ->getForm();
+        
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $email = (new Email())
+            ->from($data['email'])
+            ->to('contact@chrisdev.fr')
+            // ->text('Sending emails is fun again!')
+            ->html($data['message']);
+
+            $mailer->send($email);
+
+            $this->addFlash(
+                'success',
+                'Votre email à bien été envoyé !'
+            );
+
+            return $this->redirectToRoute('contact');
+        }
+
+        return $this->render('contact/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
