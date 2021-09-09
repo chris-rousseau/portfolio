45f0431c2a27d1d164ad26503021293405cf01ae
chris-rousseau <c.rousseau36@gmail.com>
2021-09-09 15:15:21 +0200

    Spelling correction

--- src/Controller/Admin/TagsController.php
+++ src/Controller/Admin/TagsController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Tags;
+use App\Repository\TagsRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -12,7 +13,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class TagsController extends AbstractController
 {
     /**
-     * @Route("/tags/add/ajax/{label}", name="tags_add", methods={"POST"})
+     * @Route("/tags/add/ajax/{label}", name="admin_tags_add", methods={"POST"})
      */
     public function addTags(string $label, EntityManagerInterface $em): Response
     {
@@ -25,5 +26,34 @@ class TagsController extends AbstractController
         $id = $tag->getId();
 
         return new JsonResponse(['id' => $id]);
+    }
+
+    /**
+     * @Route("admin/tags/list", name="admin_tags_list", methods={"GET"})
+     */
+    public function list(TagsRepository $tagsRepository): Response
+    {
+        $allTags = $tagsRepository->findAll();
+
+        return $this->render('admin/tags/list.html.twig', [
+            'allTags' => $allTags,
+        ]);
+    }
+
+    /**
+     * @Route("admin/tags/delete-{id}", name="admin_tags_delete", requirements={"id"="\d+"})
+     */
+    public function delete(Tags $tags): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($tags);
+        $em->flush();
+
+        $this->addFlash(
+            'danger',
+            'Le tag à bien été supprimé'
+        );
+
+        return $this->redirectToRoute('admin_tags_list');
     }
 }
